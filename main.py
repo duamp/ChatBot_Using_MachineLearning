5917c7cb7349fec805250e63f93af7d1ddac125e
Mp6 <matteo.pulcini19@gmail.com>
2021-08-29 17:36:13 -0400

    Third Commit

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from nltk.stem.lancaster import LancasterStemmer
 stemmer = LancasterStemmer()
 
 import numpy 
-import tensorflow
+import tensorflow as tf
 import tflearn
 import random 
 import json
@@ -20,13 +20,13 @@ for intent in data["intents"]:
     for pattern in intent["patterns"]:
         wrds = nltk.word_tokenize(pattern)
         words.extend(wrds)
-        docs_x.append(pattern)
+        docs_x.append(wrds)
         docs_y.append(intent["tag"]) 
 
     if intent["tag"] not in classes:
         classes.append(intent["tag"])
 
-words = [stemmer.stem(w.lower()) for w in words] #what is stemmer.stem
+words = [stemmer.stem(w.lower()) for w in words if w != "?"] #what is stemmer.stem
 words = sorted(list(set(words)))
 
 classes = sorted(classes) #sorted tags 
@@ -36,7 +36,7 @@ output = []
 
 out_empty = [0 for _ in range(len(classes))] #?
 
-for x, doc in enumerate(docs_x): #?
+for x, doc in enumerate(docs_x): # x = position & docs_x = text
     bag = []
     wrds = [stemmer.stem(w) for w in doc]
 
@@ -46,13 +46,25 @@ for x, doc in enumerate(docs_x): #?
         else:
             bag.append(0)
 
-    output_row = out_empty[:] #?
+    output_row = out_empty[:] #[:] = all elements of the array
     output_row[classes.index(docs_y[x])] = 1
 
     training.append(bag)
     output.append(output_row)
 
 traning = numpy.array(training)
-output = np.array(output)
+output = numpy.array(output)
 
+tf.reset_default_graph()
+
+net = tflearn.input_data(shape=[None, len(training[0])]) #input data (length of training data)
+net = tflearn.fully_connected(net,8) # hidden layer (8 nuerons, fully connected)
+net = tflearn.fully_connected(net,8) # hidden layer (8 nuerons, fully connected)
+net = tflearn.fully_connected(net,len(output[0]),activation="softmax") # output layer connects nueron with classes
+net = tflearn.regression(net) #
+
+model = tflearn.DNN(net) #DNN = type of nueral network 
+
+model.fit(training,output,n_epoch=1000,batch_size = 8,show_metric=True) #start passing training data, n_epoch = how many times it sees data
+model.save("model.tflearn")
 
